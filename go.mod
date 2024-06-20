3a888e5967e9901bee5144d53132149847a15a16
Almir Kadric <github@almirkadric.com>
2024-06-20 16:31:57 +0900

    added missing visualization parameters

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/AlmirKadric/terraform-provider-redash
 go 1.20
 
 require (
-	github.com/AlmirKadric/redash-client-go v0.6.6
+	github.com/AlmirKadric/redash-client-go v0.6.8
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.29.0
+	github.com/samber/lo v1.39.0
 )
 
 require (
@@ -49,11 +50,13 @@ require (
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/zclconf/go-cty v1.14.1 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	golang.org/x/mod v0.18.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/tools v0.22.0 // indirect
+	golang.org/x/tools/cmd/cover v0.1.0-deprecated // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231030173426-d783a09b4405 // indirect
 	google.golang.org/grpc v1.59.0 // indirect
--- terraform-provider-redash/resource_visualization.go
+++ terraform-provider-redash/resource_visualization.go
@@ -5,6 +5,7 @@ import (
 	"github.com/AlmirKadric/redash-client-go/redash"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/samber/lo"
 	"strconv"
 )
 
@@ -30,6 +31,295 @@ func resourceRedashVisualization() *schema.Resource {
 			"description": {
 				Type:     schema.TypeString,
 				Optional: true,
+			},
+			"chart_options": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"global_series_type": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"column_mapping": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"column": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"axis": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+								},
+							},
+						},
+						"legend": {
+							Type:     schema.TypeList,
+							Optional: true,
+							MaxItems: 1,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"enabled": {
+										Type:     schema.TypeBool,
+										Required: true,
+									},
+									"placement": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+								},
+							},
+						},
+						"series": {
+							Type:     schema.TypeList,
+							Optional: true,
+							MaxItems: 1,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"stacking": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+								},
+							},
+						},
+						"missing_values_as_zero": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"x_axis": {
+							Type:     schema.TypeList,
+							Optional: true,
+							MaxItems: 1,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"type": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"opposite": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"labels": {
+										Type:     schema.TypeList,
+										Optional: true,
+										MaxItems: 1,
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"enabled": {
+													Type:     schema.TypeBool,
+													Optional: true,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						"sort_x": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"y_axis": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"type": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"opposite": {
+										Type:     schema.TypeBool,
+										Optional: true,
+									},
+									"labels": {
+										Type:     schema.TypeList,
+										Optional: true,
+										MaxItems: 1,
+										Elem: &schema.Resource{
+											Schema: map[string]*schema.Schema{
+												"enabled": {
+													Type:     schema.TypeBool,
+													Optional: true,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						"series_options": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"name": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"z_index": {
+										Type:     schema.TypeInt,
+										Required: true,
+									},
+									"index": {
+										Type:     schema.TypeInt,
+										Required: true,
+									},
+									"type": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"y_axis": {
+										Type:     schema.TypeInt,
+										Required: true,
+									},
+								},
+							},
+						},
+						"show_data_labels": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+						"number_format": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"percent_format": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"date_time_format": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"text_format": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"table_options": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"items_per_page": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"columns": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									// Shared
+									"visible": {
+										Type:     schema.TypeBool,
+										Required: true,
+									},
+									"name": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"title": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"align_content": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"allow_search": {
+										Type:     schema.TypeBool,
+										Required: true,
+									},
+									"type": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"display_as": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"order": {
+										Type:     schema.TypeInt,
+										Required: true,
+									},
+									// Text
+									"allow_html": {
+										Type:     schema.TypeBool,
+										Required: true,
+									},
+									"highlight_links": {
+										Type:     schema.TypeBool,
+										Required: true,
+									},
+									// Number
+									"number_format": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									// Date/Time
+									"date_time_format": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									// Boolean
+									"boolean_values": {
+										Type:     schema.TypeList,
+										Required: true,
+										Elem: &schema.Schema{
+											Type: schema.TypeString,
+										},
+									},
+									// Link
+									"link_url_template": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"link_text_template": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"link_open_in_new_tab": {
+										Type:     schema.TypeBool,
+										Required: true,
+									},
+									"link_title_template": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									// Image
+									"image_url_template": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"image_width": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"image_height": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"image_title_template": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+								},
+							},
+						},
+					},
+				},
 			},
 		},
 	}
@@ -63,12 +353,119 @@ func resourceRedashVisualizationCreate(_ context.Context, d *schema.ResourceData
 
 	var diags diag.Diagnostics
 
+	var vOptions redash.VisualizationOptions
+	vType := d.Get("type").(string)
+	switch vType {
+	case "CHART":
+		chartOptions := d.Get("chart_options").([]interface{})[0].(map[string]interface{})
+		chartLegend := chartOptions["legend"].(map[string]interface{})
+		chartSeries := chartOptions["series"].(map[string]interface{})
+		chartXAxis := chartOptions["x_axis"].([]interface{})[0].(map[string]interface{})
+		chartYAxis := chartOptions["y_axis"].([]interface{})
+		chartSeriesOptions := chartOptions["series_options"].([]interface{})
+		vOptions = redash.VisualizationOptions{
+			// General
+			GlobalSeriesType: chartOptions["globalSeriesType"].(string),
+			ColumnMapping:    chartOptions["columnMapping"].(map[string]string),
+			Legend: redash.VisualizationLegendOptions{
+				Enabled:   chartLegend["enabled"].(bool),
+				Placement: chartLegend["placement"].(string),
+			},
+			Series: redash.Series{
+				Stacking: chartSeries["stacking"].(string),
+			},
+			MissingValuesAsZero: chartOptions["missingValuesAsZero"].(bool),
+			// CHART TYPE - X-Axis
+			XAxis: redash.VisualizationAxisOptions{
+				Type:     chartXAxis["type"].(string),
+				Opposite: chartXAxis["opposite"].(bool),
+				Labels: redash.VisualizationLabelOptions{
+					Enabled: chartXAxis["labels"].(map[string]interface{})["enabled"].(bool),
+				},
+			},
+			SortX: chartOptions["sortX"].(bool),
+			// CHART TYPE - Y-Axis
+			YAxis: lo.Map(chartYAxis, func(item interface{}, _ int) redash.VisualizationAxisOptions {
+				yAxis := item.(map[string]interface{})
+
+				return redash.VisualizationAxisOptions{
+					Type:     yAxis["type"].(string),
+					Opposite: yAxis["opposite"].(bool),
+					Labels: redash.VisualizationLabelOptions{
+						Enabled: yAxis["labels"].(map[string]interface{})["enabled"].(bool),
+					},
+				}
+			}),
+			// CHART TYPE - Series
+			SeriesOptions: lo.Associate(chartSeriesOptions, func(value interface{}) (string, redash.SeriesOptions) {
+				seriesOption := value.(map[string]interface{})
+
+				return seriesOption["name"].(string), redash.SeriesOptions{
+					ZIndex: seriesOption["z_index"].(int),
+					Index:  seriesOption["index"].(int),
+					Type:   seriesOption["type"].(string),
+					YAxis:  seriesOption["y_axis"].(int),
+				}
+			}),
+			// CHART TYPE - Colors
+			// CHART TYPE - Data Labels
+			ShowDataLabels: chartOptions["showDataLabels"].(bool),
+			NumberFormat:   chartOptions["numberFormat"].(string),
+			PercentFormat:  chartOptions["percentFormat"].(string),
+			DateTimeFormat: chartOptions["dateTimeFormat"].(string),
+			TextFormat:     chartOptions["textFormat"].(string),
+		}
+		break
+	case "TABLE":
+		tableOptions := d.Get("table_options").([]interface{})[0].(map[string]interface{})
+		tableColumns := tableOptions["columns"].([]interface{})
+		vOptions = redash.VisualizationOptions{
+			ItemsPerPage: tableOptions["items_per_page"].(int),
+			Columns: lo.Map(tableColumns, func(item interface{}, _ int) redash.VisualizationColumnOptions {
+				column := item.(map[string]interface{})
+				return redash.VisualizationColumnOptions{
+					// Shared
+					Visible:      column["visible"].(bool),
+					Name:         column["name"].(string),
+					Title:        column["title"].(string),
+					AlignContent: column["alignContent"].(string),
+					AllowSearch:  column["allowSearch"].(bool),
+					Type:         column["type"].(string),
+					DisplayAs:    column["displayAs"].(string),
+					Order:        column["order"].(int),
+					// Text
+					AllowHTML:      column["allowHTML"].(bool),
+					HighlightLinks: column["highlightLinks"].(bool),
+					// Number
+					NumberFormat: column["numberFormat"].(string),
+					// Date/Time
+					DateTimeFormat: column["dateTimeFormat"].(string),
+					// Boolean
+					BooleanValues: column["booleanValues"].([]string),
+					// Link
+					LinkUrlTemplate:   column["linkUrlTemplate"].(string),
+					LinkTextTemplate:  column["linkTextTemplate"].(string),
+					LinkOpenInNewTab:  column["linkOpenInNewTab"].(bool),
+					LinkTitleTemplate: column["linkTitleTemplate"].(string),
+					// Image
+					ImageUrlTemplate:   column["imageUrlTemplate"].(string),
+					ImageWidth:         column["imageWidth"].(string),
+					ImageHeight:        column["imageHeight"].(string),
+					ImageTitleTemplate: column["imageTitleTemplate"].(string),
+				}
+			}),
+		}
+		break
+	default:
+		return diag.Errorf("Invalid visualization type: %s", vType)
+	}
+
 	payload := redash.VisualizationCreatePayload{
 		QueryId:     d.Get("query_id").(int),
 		Name:        d.Get("name").(string),
 		Type:        d.Get("type").(string),
 		Description: d.Get("description").(string),
-		Options:     redash.VisualizationOptions{},
+		Options:     vOptions,
 	}
 	visualization, err := c.CreateVisualization(&payload)
 	if err != nil {
@@ -91,7 +488,10 @@ func resourceRedashVisualizationUpdate(_ context.Context, d *schema.ResourceData
 	}
 
 	payload := redash.VisualizationUpdatePayload{
-		Name: d.Get("name").(string),
+		Name:        d.Get("name").(string),
+		Type:        d.Get("type").(string),
+		Description: d.Get("description").(string),
+		Options:     redash.VisualizationOptions{},
 	}
 	visualization, err := c.UpdateVisualization(id, &payload)
 	if err != nil {
